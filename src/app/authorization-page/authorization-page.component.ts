79feae560196ec0a698907f66c85595d1e522581
Wizari <wizaripost@gmail.com>
2023-01-11 19:26:01 +0300

    add saveTokenInLocalStorage, auto authorization

--- src/app/authorization-page/authorization-page.component.ts
+++ src/app/authorization-page/authorization-page.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Component, Injectable, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
+import {AppComponent} from "../component/app.component";
 
 @Component({
   selector: 'app-authorization-page',
   templateUrl: './authorization-page.component.html',
   styleUrls: ['./authorization-page.component.scss']
 })
-export class AuthorizationPageComponent {
+@Injectable({providedIn: 'root'})
+export class AuthorizationPageComponent implements OnInit{
 
   enUrl = environment.api.url;
+  token = ``;
+  authenticated = false;
+  userName: string = ``;
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -19,18 +24,49 @@ export class AuthorizationPageComponent {
     }),
   };
 
-  constructor(private http: HttpClient) {
-  }
-
-  profileForm = new FormGroup ({
+  profileForm = new FormGroup({
     login: new FormControl('i.simagin'),
     password: new FormControl('123456')
   });
 
+  constructor(private http: HttpClient,
+              private appComponent: AppComponent,) {
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem("token") != null) {
+      // this.token = localStorage.getItem("token")!+`a`;
+      this.token = localStorage.getItem("token")!;
+      this.getInfo().subscribe(
+        result => {
+          this.authenticated = result.authenticated
+          this.userName = result.name
+          this.appComponent.userAuthorized = this.authenticated
+        },
+        error => {
+          // this.errors = error;
+          console.warn(error)
+        },
+        () => {
+          // 'onCompleted' callback.
+          // No errors, route to new page here
+        }
+      );
+    }
+  }
+
   onSubmit() {
+    console.log(localStorage.getItem("token"))
     this.postAuthenticate().subscribe(value => {
-      return console.log(value);
+      this.token = value.token;
+      this.saveTokenInLocalStorage()
+      this.getInfo().subscribe(value => {
+        this.authenticated = value.authenticated
+        this.userName = value.name
+        this.appComponent.userAuthorized = this.authenticated
+      })
     })
+    console.log(localStorage.getItem("token"))
   }
 
   postAuthenticate(): Observable<any> {
@@ -40,5 +76,16 @@ export class AuthorizationPageComponent {
     }, this.httpOptions)
   }
 
+  getInfo(): Observable<any> {
+    return this.http.get(`${this.enUrl}/info`, {
+      headers: new HttpHeaders({
+        "Authorization": `Bearer ` + this.token,
+      })
+    })
+  }
+
+  saveTokenInLocalStorage() {
+    localStorage.setItem("token", this.token);
+  }
 
 }
