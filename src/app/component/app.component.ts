4194bd2a86582520cfb593c657f845bbb9ca23a2
Wizari <wizaripost@gmail.com>
2023-01-11 19:05:24 +0300

    info get request and login screen

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+@Injectable({providedIn: 'root'})
 export class AppComponent {
   title = 'en8-my-front';
   userAuthorized: boolean = false
   // userAuthorized: boolean = true
+
 }
